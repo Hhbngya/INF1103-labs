383f730659cb1340d26bd30c0d5741a51fd896b6
Jiayi <2603644@sit.singaporetech.edu.sg>
2026-09-10 23:02:11 +0800

    Added Profile Variables

--- hello.py
+++ hello.py
@@ -2,3 +2,22 @@ print("========================")
 print("Welcome here")
 print("My first post!")
 print("========================")
+#Question 3a: Yes, the program display messages exactly as written.
+#             Whatever text that is inside the print("")is ouput.
+#Question 3b: Top to bottom. Python executes statements sequentially in the order they appear in the file.
+#Question 3c: The output appear in the integrated terminal panel at the bottom of the VS code.
+
+username = "cool_creator"
+bio = "Fun Blogger"
+followers = 100
+
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+#Question 2a: They're containers holding the data that makes up the profile;
+# username and bio hold text (strings), followers holds a number (integer).
+# Store them in variables such that if the value changes, I only need to update in one place
+#Question 2b: If I change the values, the output will change. 
+# Since print("Username:", username)reads whatever username currently holds at the moment the line runs.
+
